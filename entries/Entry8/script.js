0becf48ee9bffb347555c749da249d23166f5125
Nancyyyhu <huy291@newschool.edu>
2023-11-09 20:27:13 -0500

    change index

---
+++ entries/Entry8/script.js
@@ -0,0 +1,59 @@
+function toggleParagraph(paragraphId) {
+            var paragraph = document.getElementById(paragraphId);
+            paragraph.style.display = (paragraph.style.display === "none" || paragraph.style.display === "") ? "block" : "none";
+        }
+
+        function showImage1(imageSrc, hiddenImageId) {
+            var hiddenImage = document.getElementById(hiddenImageId);
+            hiddenImage.src = imageSrc;
+            hiddenImage.style.display = 'block';
+        }
+
+        function hideImage1(hiddenImageId) {
+            var hiddenImage = document.getElementById(hiddenImageId);
+            hiddenImage.style.display = 'none';
+        }
+
+        function showImage2(imageSrc, hiddenImageId) {
+            var hiddenImage = document.getElementById(hiddenImageId);
+            hiddenImage.src = imageSrc;
+            hiddenImage.style.display = 'block';
+        }
+
+        function hideImage2(hiddenImageId) {
+            var hiddenImage = document.getElementById(hiddenImageId);
+            hiddenImage.style.display = 'none';
+        }
+
+        function showImage3(imageSrc, hiddenImageId) {
+            var hiddenImage = document.getElementById(hiddenImageId);
+            hiddenImage.src = imageSrc;
+            hiddenImage.style.display = 'block';
+        }
+
+        function hideImage3(hiddenImageId) {
+            var hiddenImage = document.getElementById(hiddenImageId);
+            hiddenImage.style.display = 'none';
+        }
+
+        function showImage4(imageSrc, hiddenImageId) {
+            var hiddenImage = document.getElementById(hiddenImageId);
+            hiddenImage.src = imageSrc;
+            hiddenImage.style.display = 'block';
+        }
+
+        function hideImage4(hiddenImageId) {
+            var hiddenImage = document.getElementById(hiddenImageId);
+            hiddenImage.style.display = 'none';
+        }
+
+        function showImage5(imageSrc, hiddenImageId) {
+            var hiddenImage = document.getElementById(hiddenImageId);
+            hiddenImage.src = imageSrc;
+            hiddenImage.style.display = 'block';
+        }
+
+        function hideImage5(hiddenImageId) {
+            var hiddenImage = document.getElementById(hiddenImageId);
+            hiddenImage.style.display = 'none';
+        }
